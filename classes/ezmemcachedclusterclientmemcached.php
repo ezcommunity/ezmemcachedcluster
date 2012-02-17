a732ec4e6d2d0cfe9e744e44aab8f7bec0982b49
Jérôme Vieilledent <lolautruche@gmail.com>
2012-02-17 12:18:57 +0100

    Implemented Memcached client initialize method

--- classes/ezmemcachedclusterclientmemcached.php
+++ classes/ezmemcachedclusterclientmemcached.php
@@ -15,6 +15,13 @@
 class eZMemcachedClusterClientMemcached implements eZMemcachedClusterClient
 {
     /**
+     * Memcached gateway
+     *
+     * @var Memcached
+     */
+    protected $gateway;
+
+    /**
      * Constructor.
      * An exception will be thrown if Memcached PECL extension is not installed.
      *
@@ -34,6 +41,39 @@ class eZMemcachedClusterClientMemcached implements eZMemcachedClusterClient
      */
     public function initialize( eZMemcachedClusterOptions $options )
     {
+        if ( $options->usePersistentConnection && $options->connectionIdentifier != '' )
+            $this->gateway = new Memcached( $options->connectionIdentifier );
+        else
+            $this->gateway = new Memcached;
 
+        // Now set options
+        $this->gateway->setOption( Memcached::OPT_COMPRESSION, $options->useCompression );
+        $this->gateway->setOption( Memcached::OPT_BUFFER_WRITES, $options->useBuffer );
+        $this->gateway->setOption( Memcached::OPT_BINARY_PROTOCOL, $options->useBinaryProtocol );
+        if ( $options->prefixKey )
+            $this->gateway->setOption( Memcached::OPT_PREFIX_KEY, $options->prefixKey );
+
+        // Connect to Memcached backend
+        if ( $options->connectTimeout )
+            $this->gateway->setOption( Memcached::OPT_CONNECT_TIMEOUT, $options->connectTimeout );
+        $servers = array();
+        foreach ( $options->servers as $serverSpec )
+        {
+            $weight = null;
+            if ( strpos( $serverSpec, ';' ) !== false )
+                list( $serverSpec, $weight ) = explode( ';', $serverSpec );
+
+            list( $host, $port ) = explode( ':', $serverSpec );
+            $server = array( $host, (int)$port );
+            if ( isset( $weight ) )
+                $server[] = $weight;
+
+            $servers[] = $server;
+        }
+
+        if ( !$this->gateway->addServers( $servers ) )
+        {
+            eZDebug::writeError( 'A problem occurred while adding Memcached servers to client', __METHOD__ );
+        }
     }
 }
