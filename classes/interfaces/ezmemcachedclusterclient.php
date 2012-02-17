4a1caca020cc8972715831411ff1e6fe5c7ddf2d
Jérôme Vieilledent <lolautruche@gmail.com>
2012-02-17 12:18:29 +0100

    Updated memcached client interface

--- classes/interfaces/ezmemcachedclusterclient.php
+++ classes/interfaces/ezmemcachedclusterclient.php
@@ -19,4 +19,44 @@ interface eZMemcachedClusterClient
      * @return void
      */
     public function initialize( eZMemcachedClusterOptions $options );
+
+    /**
+     * Gets a cached item identified by $key.
+     * Returned value depends on what has been stored previously.
+     * False will be returned if $key has not been found in Memcached backend.
+     *
+     * @param string $key
+     * @return mixed
+     */
+    public function get( $key );
+
+    /**
+     * Caches an item in Memcached backend
+     *
+     * @param string $key Label for the value to store
+     * @param mixed $value The value you want to store. Can be anything but a resource
+     * @param int $ttl Time to live for this key/value pair. Can be:
+     *                 - A relative value (in seconds), but cannot exceed 30 days
+     *                 - A UNIX timestamp
+     *                 - If set to 0, the cached value will never expire
+     *
+     * @return void
+     */
+    public function set( $key, $value, $ttl );
+
+    /**
+     * Deletes a cached item identified by $key
+     *
+     * @param string $key
+     * @return void
+     */
+    public function delete( $key );
+
+    /**
+     * Flushes all cache from Memcached backend
+     *
+     * @param int $delay Delay before flushing server, in seconds.
+     * @return void
+     */
+    public function flush( $delay = 0 );
 }
